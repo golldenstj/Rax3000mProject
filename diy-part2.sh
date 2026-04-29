25af6f618f9c48da49b36d6b26e47d45d20cad20
golldenstj <70574688+golldenstj@users.noreply.github.com>
2026-04-29 16:06:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -58,6 +58,24 @@ rm -rf OldPackages.zip packages-0f7be9fc93d68986c179829d8199824d3183eb60s
 sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.68.1/' feeds/packages/net/frp/Makefile
 sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=44ed7107bf35e4f68dc0e77cd5805102effa5301528b89ee5ab0ab379088edc6/' feeds/packages/net/frp/Makefile
 
+# 加 noweb 标签规避v0.67之后的变动
+sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.68.1/' feeds/packages/net/frp/Makefile
+sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=44ed7107bf35e4f68dc0e77cd5805102effa5301528b89ee5ab0ab379088edc6/' feeds/packages/net/frp/Makefile
+
+grep -q 'GO_PKG_TAGS:=noweb' feeds/packages/net/frp/Makefile || \
+sed -i '/GO_PKG_BUILD_PKG:=github.com\/fatedier\/frp\/cmd\/\.\.\./a GO_PKG_TAGS:=noweb' feeds/packages/net/frp/Makefile
+
+grep -q 'web/frpc/dist' feeds/packages/net/frp/Makefile || cat >> feeds/packages/net/frp/Makefile <<'EOF'
+
+define Build/Prepare
+	$(call Build/Prepare/Default)
+	mkdir -p $(PKG_BUILD_DIR)/web/frpc/dist
+	mkdir -p $(PKG_BUILD_DIR)/web/frps/dist
+	touch $(PKG_BUILD_DIR)/web/frpc/dist/.keep
+	touch $(PKG_BUILD_DIR)/web/frps/dist/.keep
+endef
+EOF
+
 # 删除克隆的 coolsnowwolf-luci 和 coolsnowwolf-packages 仓库
 rm -rf coolsnowwolf-luci
 rm -rf coolsnowwolf-packages
